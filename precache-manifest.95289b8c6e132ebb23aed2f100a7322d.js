2e8e063c408dfa8a537df7d162ae138e59a1c0f1
ritwik4690 <vaidyaritwik@gmail.com>
2021-02-14 14:21:04 +0530

    Updates

---
+++ precache-manifest.95289b8c6e132ebb23aed2f100a7322d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bac9507faaff861b929280e5cdc769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4cfde090445c7cd31a1",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "988bc669b5daf9d7d27a",
+    "url": "/static/js/2.70d66a62.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.70d66a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4cfde090445c7cd31a1",
+    "url": "/static/js/main.78c8d889.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "238abea6b465fbacff3451031a9e1f74",
+    "url": "/static/media/resume.238abea6.pdf"
+  }
+]);
